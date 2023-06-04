ccbbc726b50565c04eb92577a6c0682ca1aaf305
WalkerTian <129571444+Walkersifolia@users.noreply.github.com>
2023-06-04 19:59:42 +0800

    Add files via upload

--- apps/help.js
+++ apps/help.js
@@ -1,6 +1,7 @@
 import plugin from '../../../lib/plugins/plugin.js';
+import { segment } from 'icqq';
 
-const content =
+const helptext =
   '欢迎使用PhigorsLibrary-Plugn\n' +
   '「/p help」获取本帮助\n' +
   '「/p bind xxxxxxxxxx」绑定25位的SessionToken，不建议群聊使用\n' +
@@ -31,10 +32,14 @@ export class phigros extends plugin{
     }
 
     async help () {
-        await this.reply(content)
+        await this.reply(helptext)
     }
 
     async getstk () {
-        await this.reply('http://qxsky.top:886/externalLinksController/chain/getstk.apk?ckey=UbcekU4SrbrP56nuuJsjSG4sR6XVva0QpH6cgRxykQ%2BLVKfVVy1N9ftDKol27wSM')
+        const url = 'http://qxsky.top:886/externalLinksController/chain/getstk.apk?ckey=UbcekU4SrbrP56nuuJsjSG4sR6XVva0QpH6cgRxykQ%2BLVKfVVy1N9ftDKol27wSM'
+        const title = '点此下载获取SessionToken所需的apk'
+        const image = 'http://qxsky.top:89/img/icon.png'
+        const content = '圈圈'
+        await this.reply(segment.share(url,title,image,content))
     }
 }
