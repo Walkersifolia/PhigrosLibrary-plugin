f70e93415984c21b392ec56f61d718e88bfe3378
WalkerTian <129571444+Walkersifolia@users.noreply.github.com>
2023-06-09 20:25:57 +0800

    基于3.1.0的PhigrosLibrary实现了获取玩家ID的功能而不是群昵称

--- apps/b19.js
+++ apps/b19.js
@@ -1,6 +1,7 @@
 import plugin from '../../../lib/plugins/plugin.js';
 import fs from 'node:fs';
 import puppeteer from '../../../lib/puppeteer/puppeteer.js';
+import fetch from 'node-fetch';
 
 export class phigros extends plugin{
     constructor() {
@@ -18,33 +19,31 @@ export class phigros extends plugin{
         })
     }
 
-    
-
     async b19 (e) {
         const userid = `${e.user_id}`
         //Node.js的uncaughtException事件处理程序
         process.on("uncaughtException", function (err) {
             logger.erro(err)
         })
-
         //检测是否绑定SessionToken，如未绑定，提示绑定
         try {
             const test = fs.readFileSync(`./plugins/PhigrosLibrary-plugin/data/userdata/${userid}.js`, 'utf-8');
         } catch (err) {
             e.reply('尚未绑定SessionToken，请发送“/p bind xxxxxxxxxx”来绑定Phigros账号')
         }
-        
         //读取用户SessionToken数据
         const sessiontoken = fs.readFileSync(`./plugins/PhigrosLibrary-plugin/data/userdata/${userid}.js`, 'utf-8');
-
         logger.info(sessiontoken)
-
+        //获取玩家ID
+        const player = await fetch(`http://127.0.0.1:9090/playerId/${sessiontoken}`)
+        const ID = await player.text()
+        logger.info(ID)
         // 使用 puppeteer 库初始化浏览器
         const browser = await puppeteer.browserInit();
         // 打开一个新页面
         const page = await browser.newPage();
         // 导航到特定 URL
-        await page.goto('http://127.0.0.1:9091/index.html?name=' + encodeURIComponent(e.nickname) + '#' + sessiontoken);
+        await page.goto('http://127.0.0.1:9091/index.html?name=' + ID + '#' + sessiontoken);
         // 截取页面图像
         setTimeout(async () => {
             await page.setViewport({ width: 1030, height: 3670 });
