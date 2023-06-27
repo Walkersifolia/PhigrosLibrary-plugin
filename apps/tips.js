3a56d502c7fbad1ce9c0fc1c4795a753738c80a5
YuYue <86163024+YuYue-Amatsuki@users.noreply.github.com>
2023-06-28 01:11:58 +0800

    我是傻逼（

--- apps/tips.js
+++ apps/tips.js
@@ -2,27 +2,27 @@ import plugin from '../../../lib/plugins/plugin.js';
 import fs from 'fs';
 import path from 'path';
 
-export class RandomTips extends plugin {
+export class RandomTip extends plugin {
     constructor() {
         super({
-            name: '随机tips',
-            dsc: '随机tips',
+            name: '随机tip',
+            dsc: '随机tip',
             event: 'message',
             priority: 5000,
             rule: [{
-                reg: '^/p tips$',
-                fnc: 'tips'
+                reg: '^/p tip$',
+                fnc: 'tip'
             }]
         })
     }
 
-    async tips(e) {
+    async tip(e) {
         const filePath = new URL('../resources/tips.txt', import.meta.url);
         
         const tips = fs.readFileSync(filePath, 'utf-8').split('\n').filter(line => line.trim() !== '');
         
-        const randomIndex = Math.floor(Math.random() * tips.length);
-        const randomTip = tips[randomIndex].trim();
+        const randomIndex = Math.floor(Math.random() * tip.length);
+        const randomTip = tip[randomIndex].trim();
 
         e.reply("Tip:" + randomTip);
         return true;
